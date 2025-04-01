a97444b8f98fd676e2c01b9a03114da6c499d8ad
Virender Rana <143004956+Virender-45@users.noreply.github.com>
2025-04-01 10:40:17 +0530

    Update TicTacToe.java

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -101,6 +101,17 @@ public class TicTacToe {
 				return;
 			}
 		}
+		
+		//diagnols
+		if(board[0][0].getText() != "" &&
+				board[0][0].getText() == board[1][1].getText() &&
+				board[1][1].getText() == board[2][2].getText()) {
+			for(int i = 0; i < 3; i++) {
+				setWinner(board[i][i]);
+			}
+			gameOver = true;
+			return;
+		}
 	}
 	void setWinner(JButton tile) {
 		tile.setForeground(Color.green);
